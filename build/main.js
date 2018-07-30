df11a565b55b8ffe5039312c53e6e50c1a3fdf18
DavidOConnor <david.alan.oconnor@gmail.com>
2018-07-29 23:56:05 -0400

    update

--- build/main.js
+++ build/main.js
@@ -163,7 +163,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./src/from_rust_bg.wasm":"b0a7d4ff85842532b2a9"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./src/from_rust_bg.wasm":"f3b0e6839c73b40a22e0"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
